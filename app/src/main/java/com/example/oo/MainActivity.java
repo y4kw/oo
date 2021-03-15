28a85437a6e5de29ac65dc8569cdb8b7628e1569
y4kw <y4kw@math.sci.hokudai.ac.jp>
2021-03-15 17:09:50 +0900

    runnable

--- app/src/main/java/com/example/oo/MainActivity.java
+++ app/src/main/java/com/example/oo/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.oo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.WallpaperInfo;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.Browser;
+
+import java.security.AccessController;
 
 public class MainActivity extends AppCompatActivity {
-    String pdfUrl = "http://www.data.jma.go.jp/fcd/yoho/data/jishin/kaisetsu_tanki_latest.pdf";
+    //String url = "http://www.data.jma.go.jp/fcd/yoho/data/jishin/kaisetsu_tanki_latest.pdf";
+    String url = "http://www.jma.go.jp/bosai/map.html#4/28.536/123.486/&elem=vap&contents=himawari";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +24,10 @@ public class MainActivity extends AppCompatActivity {
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
             Intent intent = new Intent();
             intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            intent.putExtra(Browser.EXTRA_APPLICATION_ID, "com.android.chrome");
+            //intent.putExtra(Browser.EXTRA_APPLICATION_ID, context.getPackageName());
             intent.setAction(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse(pdfUrl));
+            intent.setData(Uri.parse(url));
             startActivity(intent);
             finish();
         }, 3000);
