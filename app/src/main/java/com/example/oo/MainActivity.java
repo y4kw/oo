8cedb92cfd91f80400977c92dff5e6b1b90b5da1
y4kw <y4kw@math.sci.hokudai.ac.jp>
2021-03-16 13:58:50 +0900

    customtabs

--- app/src/main/java/com/example/oo/MainActivity.java
+++ app/src/main/java/com/example/oo/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.oo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
 
 import android.app.WallpaperInfo;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -15,22 +17,29 @@ import java.security.AccessController;
 
 public class MainActivity extends AppCompatActivity {
     //String url = "http://www.data.jma.go.jp/fcd/yoho/data/jishin/kaisetsu_tanki_latest.pdf";
-    String url = "http://www.jma.go.jp/bosai/map.html#4/28.536/123.486/&elem=vap&contents=himawari";
+    String url = "https://www.jma.go.jp/bosai/map.html#4/28.536/123.486/&elem=vap&contents=himawari";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            Intent intent = new Intent();
-            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-            intent.putExtra(Browser.EXTRA_APPLICATION_ID, "com.android.chrome");
-            //intent.putExtra(Browser.EXTRA_APPLICATION_ID, context.getPackageName());
-            intent.setAction(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse(url));
-            startActivity(intent);
-            finish();
-        }, 3000);
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            builder.setToolbarColor(Color.parseColor("#0099cc"));
+            builder.setShowTitle(true);
+            //builder.enableUrlBarHiding();
+            CustomTabsIntent tabsIntent = builder.build();
+            tabsIntent.launchUrl(this, Uri.parse(url));
+
+            //Intent intent = new Intent();
+            //intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            //intent.putExtra(Browser.EXTRA_APPLICATION_ID, "com.android.chrome");
+            ////intent.putExtra(Browser.EXTRA_APPLICATION_ID, context.getPackageName());
+            //intent.setAction(Intent.ACTION_VIEW);
+            //intent.setData(Uri.parse(url));
+            //startActivity(intent);
+            //finish();
+        }, 300);
     }
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
